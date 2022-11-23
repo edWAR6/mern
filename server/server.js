7c7004accbaf2981f0a2ac091fab87575fc01a8c
Eduardo Oviedo-Blanco <eduardo.oviedo@gmail.com>
2022-11-22 19:31:44 -0600

    building a mern architechture

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+const dbo = require('./db/connection');
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  dbo.connectToServer(function(err){
+    if (err) {
+      console.error(err);
+    }
+  });
+  console.info(`Server is listening on port: ${port}`);
+});
