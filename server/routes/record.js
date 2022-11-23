7a5a43d64e75e0c81e9dae9b51393eaaa5190178
Eduardo Oviedo-Blanco <eduardo.oviedo@gmail.com>
2022-11-22 21:23:19 -0600

    record routes added

--- server/routes/record.js
+++ server/routes/record.js
@@ -6,8 +6,30 @@ const dbo = require('../db/connection');
 const ObjectId = require('mongodb').ObjectId;
 
 recordRoutes.route('/record').get(function(req, res){
-  let db_connect = dbo.getDb('employees');
+  let db_connect = dbo.getDb();
   db_connect.collection('records').find({}).toArray(function(err, result){
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+recordRoutes.route('/record/:id').get(function(req, res){
+  let db_connect = dbo.getDb();
+  let query = { _id: ObjectId(req.params.id) };
+  db_connect.collection('records').findOne(query, function(err, result){
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+recordRoutes.route('/record/add').post(function(req, res){
+  let db_connect = dbo.getDb();
+  let record = {
+    name: req.body.name,
+    position: req.body.position,
+    level: req.body.level,
+  };
+  db_connect.collection('records').insertOne(record, function(err, result){
     if (err) throw err;
     res.json(result);
   });
